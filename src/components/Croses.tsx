091c8a0b6d50ca5689541e72074e67c0e2385877
OlhaFedorets <fedoretsolha@gmail.com>
2024-11-02 12:44:04 +0300

    change croses

--- src/components/Croses.tsx
+++ src/components/Croses.tsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, {ReactNode} from 'react';
 
-export const Croses = () => {
+type Props = {
+    croses: Croses[]
+    children: ReactNode
+}
+
+type Croses = {
+    id: number
+    model: string
+    size: string
+}
+
+export const Croses = ({croses, children}: Props) => {
     return (
         <div>
-
+            <ol>
+                {croses.map((el: Croses) => {
+                    return (
+                        <li key={el.id}>
+                            <span>{el.id}</span>
+                            <span> -{el.model}</span>
+                            <span>- {el.size}</span>
+                        </li>
+                    )
+                })}
+            </ol>
+            {children}
+            <hr/>
         </div>
     );
 };
