6f7530f4f3df2f73f7cabb769e2c571f5aee3cda
OlhaFedorets <fedoretsolha@gmail.com>
2024-11-02 12:44:14 +0300

    change app

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import './App.css';
 import {Modal} from "./components/modal/Modal";
 import {Button} from "./components/Button";
 import s from './components/Button.module.css'
+import {Croses} from "./components/Croses";
+
 
 
 function App() {
@@ -30,10 +32,58 @@ function App() {
             {/*>Blue Button</Button>*/}
 
 
-            <Button onClick={() => {}} color={'red'}>Red Button</Button>
-            <Button onClick={() => {}} color={'secondary'}>Secondary Button</Button>
-            <Button onClick={() => {}}>Default Button</Button>
-            <Button onClick={() => {}} disabled>Disabled Button</Button>
+            {/*<Button onClick={() => {}} color={'red'}>Red Button</Button>*/}
+            {/*<Button onClick={() => {}} color={'secondary'}>Secondary Button</Button>*/}
+            {/*<Button onClick={() => {}}>Default Button</Button>*/}
+            {/*<Button onClick={() => {}} disabled>Disabled Button</Button>*/}
+
+            {/*<Croses croses={croses}>*/}
+            {/*    <input/>*/}
+            {/*    <Button onClick={() => {}} color={'red'}>Red Button</Button>*/}
+            {/*    <Button onClick={() => {}} color={'secondary'}>Secondary Button</Button>*/}
+            {/*    <div>text text text text text text text text text text</div>*/}
+            {/*    <div>text text text text text text text text text text</div>*/}
+            {/*    <div>text text text text text text text text text text</div>*/}
+            {/*    <div>text text text text text text text text text text</div>*/}
+            {/*    <div>text text text text text text text text text text</div>*/}
+            {/*    <div>text text text text text text text text text text</div>*/}
+            {/*    <div>text text text text text text text text text text</div>*/}
+            {/*    <Button onClick={() => {}}>Default Button</Button>*/}
+            {/*    <Button onClick={() => {}} disabled>Disabled Button</Button>*/}
+            {/*</Croses>*/}
+
+            {/*<Croses croses={croses}>*/}
+            {/*    <div>text text text text text text text text text text</div>*/}
+            {/*    <div>text text text text text text text text text text</div>*/}
+            {/*    <div>text text text text text text text text text text</div>*/}
+            {/*    <div>text text text text text text text text text text</div>*/}
+            {/*    <div>text text text text text text text text text text</div>*/}
+            {/*    <div>text text text text text text text text text text</div>*/}
+            {/*</Croses>*/}
+
+            {/*<Croses croses={croses}>*/}
+            {/*    <input/>*/}
+            {/*    <Button onClick={() => {}} color={'red'}>Red Button</Button>*/}
+            {/*    <Button onClick={() => {}} color={'secondary'}>Secondary Button</Button>*/}
+            {/*</Croses>*/}
+
+            {/*<Modal>*/}
+            {/*    <input id={'email'} placeholder={'email'}/>*/}
+            {/*    <input id={'pass'} placeholder={'pass'}/>*/}
+            {/*    <button>send</button>*/}
+            {/*</Modal>*/}
+
+            <Modal>
+                <h1>Confident information</h1>
+                <input placeholder={'email'}/>
+                <input placeholder={'pass'}/>
+                <input placeholder={'pass'}/>
+                <label>
+                    <input type={'checkbox'} />
+                    I agree
+                </label>
+                <button>send</button>
+            </Modal>
 
         </div>
     );
